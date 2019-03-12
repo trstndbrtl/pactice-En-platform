72c82f6caea021ec18dc8507c1e7d2f2dbe9c51b
trstndbrtl <trstndbrtl@2354616.no-reply.drupal.org>
2019-03-12 18:21:59 +0100

    [FEAT] Verb Entity and CRUD system

---
+++ src/Form/VerbFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Verb;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VerbFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('present')
+            ->add('preterit')
+            ->add('participe')
+            ->add('traduction')
+            ->add('phonetique')
+            ->add('irregular')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Verb::class,
+        ]);
+    }
+}
---
+++ src/Migrations/Version20190312164345.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190312164345 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE verb (id INT AUTO_INCREMENT NOT NULL, present VARCHAR(60) NOT NULL, preterit VARCHAR(60) NOT NULL, participe VARCHAR(60) NOT NULL, traduction VARCHAR(255) DEFAULT NULL, phonetique VARCHAR(255) DEFAULT NULL, irregular TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE verb');
+    }
+}
